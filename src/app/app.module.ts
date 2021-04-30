537e9578987cca9677c26c314fae25f0a23caf5d
CarlosMotaMX <201822034@tese.edu.mx>
2021-04-29 20:50:05 -0500

    Inserción de Villanos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { ListadoComponent } from './heroes/listado/listado.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ContadorComponent } from './contador/contador.component';
+
+import { VillanosComponent } from './villanos/villanos.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroeComponent,
-    ListadoComponent
-  ],
+      ContadorComponent,
+      VillanosComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
